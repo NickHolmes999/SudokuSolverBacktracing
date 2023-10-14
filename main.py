caea4fec11573e16e01fade920e5dd2d9e955815
NickHolmes999 <nickholmes2003@gmail.com>
2023-10-14 15:36:13 -0400

    Added difficulty levels, create boards from scratch

--- main.py
+++ main.py
@@ -1,38 +1,101 @@
-import pygame
+import pygame # Import pygame library
+import copy # Import copy library to make a deep copy of the grid
+import random
 
+def generate_sudoku():
+    # Starting with a solved board, you might use something like the following
+    base  = 3
+    side  = base*base
 
-pygame.font.init()
-screen = pygame.display.set_mode((600, 600))
-pygame.display.set_caption('SUDOKU SOLVER(BACKTRACING)')
-img = pygame.image.load('icon.png')
-pygame.display.set_icon(img)
-font1 = pygame.font.SysFont('comicsans', 40)
-font2 = pygame.font.SysFont('comicsans', 20)
+    # pattern for a baseline valid solution
+    def pattern(r,c): return (base*(r%base)+r//base+c)%side
+
+    # randomize rows, columns and numbers (of valid base pattern)
+    def shuffle(s): return random.sample(s,len(s))
+    rBase = range(base)
+    rows  = [ g*base + r for g in shuffle(rBase) for r in shuffle(rBase) ]
+    cols  = [ g*base + c for g in shuffle(rBase) for c in shuffle(rBase) ]
+    nums  = shuffle(range(1,base*base+1))
+
+    # produce board using randomized baseline pattern
+    return [ [nums[pattern(r,c)] for c in cols] for r in rows ]
+
+pygame.font.init() # Initialize pygame font
+screen = pygame.display.set_mode((600, 600)) # Set screen size
+pygame.display.set_caption('SUDOKU SOLVER(BACKTRACING)')    # Set title of screen
+img = pygame.image.load('icon.png') # Load icon image
+pygame.display.set_icon(img) # Set icon image
+font1 = pygame.font.SysFont('comicsans', 40) # Set font for numbers
+font2 = pygame.font.SysFont('comicsans', 20)    # Set font for instructions
 x = 0
 y = 0
 dif = 500 / 9
 val = 0
 
-# Default Sudoku Board
-grid = [
-   [7, 8, 0, 4, 0, 0, 1, 2, 0],
-   [6, 0, 0, 0, 7, 5, 0, 0, 9],
-   [0, 0, 0, 6, 0, 1, 0, 7, 8],
-   [0, 0, 7, 0, 4, 0, 2, 6, 0],
-   [0, 0, 1, 0, 5, 0, 9, 3, 0],
-   [9, 0, 4, 0, 6, 0, 0, 0, 5],
-   [0, 7, 0, 3, 0, 0, 0, 1, 2],
-   [1, 2, 0, 0, 0, 7, 4, 0, 0],
-   [0, 4, 9, 2, 0, 6, 0, 0, 7]
-]
 
+def create_puzzle(board, difficulty):
+    puzzle = copy.deepcopy(board)
+    total_cells = 81
 
-def get_coord(pos):
+    # Set the number of cells to empty, depending on the difficulty level
+    if difficulty == 'easy':
+        num_to_remove = 20
+    elif difficulty == 'medium':
+        num_to_remove = 30
+    elif difficulty == 'hard':
+        num_to_remove = 40
+
+    for _ in range(num_to_remove):
+        x = random.randint(0, 8)
+        y = random.randint(0, 8)
+        puzzle[x][y] = 0
+
+    return puzzle
+
+def select_difficulty():
+    screen.fill((255, 255, 255))
+    font = pygame.font.SysFont('comicsans', 32)
+
+    text1 = font.render('Select Difficulty', 1, (0, 0, 0))
+    text2 = font.render('1: Easy', 1, (0, 0, 0))
+    text3 = font.render('2: Medium', 1, (0, 0, 0))
+    text4 = font.render('3: Hard', 1, (0, 0, 0))
+
+    screen.blit(text1, (200, 100))
+    screen.blit(text2, (200, 200))
+    screen.blit(text3, (200, 300))
+    screen.blit(text4, (200, 400))
+
+    pygame.display.update()
+
+    selecting = True # Loop until a valid difficulty is selected
+    while selecting:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                full_board = generate_sudoku()
+                if event.key == pygame.K_1:
+                    return create_puzzle(full_board, 'easy')
+                elif event.key == pygame.K_2:
+                    return create_puzzle(full_board, 'medium')
+                elif event.key == pygame.K_3:
+                    return create_puzzle(full_board, 'hard')
+
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+# Replace the original grid variable with a function call
+grid = select_difficulty()
+
+# Make a deep copy of the original grid for reset functionality
+original_grid = copy.deepcopy(grid)
+
+def get_coord(pos): # Get the position of the mouse click
    x = pos[0] // dif
    y = pos[1] // dif
 
 
-def draw_box():
+def draw_box(): # Draw red lines to form 9x9 grid
    for i in range(2):
        pygame.draw.line(screen, (255, 0, 0), (x * dif-3, (y + i)
                         * dif), (x * dif + dif + 3, (y + i)*dif), 7)
@@ -40,7 +103,7 @@ def draw_box():
                         y * dif), ((x + i) * dif, y * dif + dif), 7)
 
 
-def draw():
+def draw(): # Draw the numbers in the grid
    for i in range(9):
        for j in range(9):
            if grid[i][j] != 0:
@@ -49,7 +112,7 @@ def draw():
                text1 = font1.render(str(grid[i][j]), 1, (0, 0, 0))
                screen.blit(text1, (i * dif + 15, j * dif + 5))
 
-   for i in range(10):
+   for i in range(10): # Draw the lines that form the 9x9 grid
        if i % 3 == 0:
            thick = 7
        else:
@@ -60,22 +123,22 @@ def draw():
                         (i * dif, 500), thick)
 
 
-def draw_val(val):
+def draw_val(val): # Draw the value entered by the user
    text1 = font1.render(str(val), 1, (0, 0, 0))
    screen.blit(text1, (x * dif + 15, y * dif + 15))
 
 
-def raise_error_1():
+def raise_error_1(): # Raise error when wrong value entered
    text1 = font1.render('WRONG !!!', 1, (0, 0, 0))
    screen.blit(text1, (20, 570))
 
 
-def raise_error_2():
+def raise_error_2(): # Raise error when wrong key is pressed
    text1 = font1.render('Not a valid Key', 1, (0, 0, 0))
    screen.blit(text1, (20, 570))
 
 
-def valid(m, i, j, val):
+def valid(m, i, j, val): # Check if the value entered is valid
    for it in range(9):
        if m[i][it] == val:
            return False
@@ -92,7 +155,7 @@ def valid(m, i, j, val):
    return True
 
 
-def solve(grid, i, j):
+def solve(grid, i, j): # Solve the sudoku using backtracking
    while grid[i][j] != 0:
        if i < 8:
            i += 1
@@ -102,7 +165,7 @@ def solve(grid, i, j):
        elif i == 8 and j == 8:
            return True
 
-   pygame.event.pump()
+   pygame.event.pump() # Process events
    for it in range(1, 10):
        if valid(grid, i, j, it) == True:
            grid[i][j] = it
@@ -127,7 +190,7 @@ def solve(grid, i, j):
    return False
 
 
-def instruction():
+def instruction(): # Display instruction to use the game
   text1 = font2.render(
       'PRESS D TO RESET TO DEFAULT / R TO EMPTY\n', 1, (0, 0, 0))
   text2 = font2.render(
@@ -136,26 +199,26 @@ def instruction():
   screen.blit(text2, (20, 540))
 
 
-def result():
+def result(): # Display the result
   text1 = font1.render('FINISHED PRESS R or D\n', 1, (0, 0, 0))
   screen.blit(text1, (20, 570))
 
 
-run = True
-flag_1 = 0
-flag_2 = 0
-rs = 0
-error = 0
-while run:
-   screen.fill((255, 255, 255))
-   for event in pygame.event.get():
-       if event.type == pygame.QUIT:
+run = True # Run the game loop
+flag_1 = 0 # Flag to check if a key is pressed
+flag_2 = 0 # Flag to check if enter is pressed
+rs = 0  # Flag to check if result is displayed
+error = 0   # Variable to check if there is an error
+while run: # Run the game loop
+   screen.fill((255, 255, 255)) # Fill the screen with white color
+   for event in pygame.event.get(): # Process events
+       if event.type == pygame.QUIT: # If user clicks close button
            run = False
-       if event.type == pygame.MOUSEBUTTONDOWN:
+       if event.type == pygame.MOUSEBUTTONDOWN: # If user clicks mouse
            flag_1 = 1
-           pos = pygame.mouse.get_pos()
+           pos = pygame.mouse.get_pos() # Get the position of the mouse click
            get_coord(pos)
-       if event.type == pygame.KEYDOWN:
+       if event.type == pygame.KEYDOWN: # If user presses a key
            if event.key == pygame.K_LEFT:
                x -= 1
                flag_1 = 1
@@ -211,26 +274,16 @@ while run:
                rs = 0
                error = 0
                flag_2 = 0
-               grid = [
-                   [7, 8, 0, 4, 0, 0, 1, 2, 0],
-                   [6, 0, 0, 0, 7, 5, 0, 0, 9],
-                   [0, 0, 0, 6, 0, 1, 0, 7, 8],
-                   [0, 0, 7, 0, 4, 0, 2, 6, 0],
-                   [0, 0, 1, 0, 5, 0, 9, 3, 0],
-                   [9, 0, 4, 0, 6, 0, 0, 0, 5],
-                   [0, 7, 0, 3, 0, 0, 0, 1, 2],
-                   [1, 2, 0, 0, 0, 7, 4, 0, 0],
-                   [0, 4, 9, 2, 0, 6, 0, 0, 7]
-               ]
+               grid = copy.deepcopy(original_grid)
 
-   if flag_2 == 1:
+   if flag_2 == 1: # Call solve function when enter is pressed
        if solve(grid, 0, 0) == False:
            error = 1
        else:
            rs = 1
        flag_2 = 0
 
-   if val != 0:
+   if val != 0: # Draw only when val is not 0
        draw_val(val)
        if valid(grid, int(x), int(y), val) == True:
            grid[int(x)][int(y)] = val
@@ -240,13 +293,13 @@ while run:
            raise_error_2()
        val = 0
 
-   if error == 1:
+   if error == 1: # If there is an error display it
        raise_error_1()
    if rs == 1:
        result()
    draw()
    if flag_1 == 1:
        draw_box()
-   instruction()
+   instruction() # Display instructions
 
-   pygame.display.update()
+   pygame.display.update() # Update the display
